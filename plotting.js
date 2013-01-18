f90ddf2133568a0ae3c6eefe7f145aa76f8ea348
OpenShift guest <totor@fake.com>
2013-01-18 17:04:35 -0500

    up

--- plotting.js
+++ plotting.js
@@ -12,8 +12,9 @@ function test2()
 {
     this.x=0.5;
     this.y=0.5;
-    this.values.x=0.5;
-    this.values.y=0.5;
+//    this.values.x=0.5;
+//    this.values.y=0.5;
+    this.series.chart.redraw();
   //  this.remove();
  //   this.series[0].addPoint([0.5,0.5]);
 //    this.x=0.5;
