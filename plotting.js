5928bfb090a7390222e49b4e5c8d0139e9624215
codec-abc <viot.camille@outlook.com>
2013-01-19 02:02:25 +0100

    up

--- plotting.js
+++ plotting.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
  
   $.jqplot.config.enablePlugins = true;
  
-  s1 = [['23-May-08',1],['24-May-08',4],['25-May-08',2],['26-May-08', 6]];
+  s1 = [[0,0],[0.33,0.33],[0.66, 0.66],[1, 1]];
  
   plot1 = $.jqplot('chart1',[s1],{
      title: 'Highlighting, Dragging, Cursor and Trend Line',
