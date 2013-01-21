b475780c26c9e27c587c26938808771118917751
OpenShift guest <totor@fake.com>
2013-01-21 08:23:23 -0500

    up

--- plotting.js
+++ plotting.js
@@ -1,23 +1,60 @@
 function computeFile()
 {
-    alert("bouton clique");
-    var toto = plot1.series[0].data[2][2];//._plotdata[0];
+    if(validData())
+    {
+    
+    }
+    else
+    {
+        alert("Points positions are not valid");
+    }
 }
 
-function graph() {
- 
+function graph() 
+{
   $.jqplot.config.enablePlugins = true;
- 
   s1 = [[0,0],[0.33,0.33],[0.66, 0.66],[1, 1]];
- 
   plot1 = $.jqplot('chart1',[s1],{
      title: 'Highlighting, Dragging, Cursor and Trend Line',
 
   });
 }
 
-
-
 $(document).ready(graph);
 
+function validData()
+{
 
+    var keep = true;
+    for(var i=0; i<3 && keep; i++)
+    {
+        var x = plot1.series[0].data[i][0];
+        var y = plot1.series[0].data[i][0];
+        
+        var xNext = plot1.series[0].data[i+1][0];
+        var yNext = plot1.series[0].data[i+1][0];
+        
+        if(xNext<= x)
+        {
+            keep=false
+        }
+        if(x<0 || x>1)
+        {
+            keep=false
+        }
+        if(y<0 || y>1)
+        {
+            keep=false
+        }
+        if(xNext<0 || xNext>1)
+        {
+            keep=false
+        }
+        if(yNext<0 || yNext>1)
+        {
+            keep=false
+        }
+        
+    }
+    return keep;
+}
